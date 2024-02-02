bf3e210e351ec1ab32f4f208b595113b0a765252
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2024-02-02 11:20:48 +0100

    routing refactored

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { CountryDataService } from "./country-search-engine/services/country-data.service";
 import { CountrySearchEngineModule } from "./country-search-engine/country-search-engine.module";
 import { ViewsModule } from "./views/views.module";
-import { routeConfig } from "./app-routing.module";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -19,7 +18,7 @@ import { routeConfig } from "./app-routing.module";
     FormsModule,
     CountrySearchEngineModule,
     ViewsModule,
-    RouterModule.forRoot(routeConfig)
+    AppRoutingModule
   ],
   providers: [HttpClientModule, CountryDataService],
   bootstrap: [AppComponent]
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -3,15 +3,16 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PageNotFoundComponent
   ],
   exports: [
-    HomeComponent,
     HeaderComponent,
     FooterComponent
   ]
