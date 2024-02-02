2b86b7930d15db83dcdb5ecd9019fb8710f3f816
Stasiek99 <stasiekpilich@gmail.com>
2024-02-02 11:19:12 +0100

    routing refactored

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
-import { Routes } from "@angular/router";
+import { NgModule} from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
 import { HomeComponent } from "./views/components/home/home.component";
 import { CountrySearchEngineComponent } from "./country-search-engine/country-search-engine/country-search-engine.component";
+import { PageNotFoundComponent } from "./views/components/page-not-found/page-not-found.component";
 
 export const routeConfig: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "search-bar", component: CountrySearchEngineComponent}
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "search-bar", component: CountrySearchEngineComponent},
+  {path: "**", pathMatch: "full", component: PageNotFoundComponent}
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routeConfig)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
