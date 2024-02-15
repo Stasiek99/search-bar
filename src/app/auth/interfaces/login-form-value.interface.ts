ff4594e00f5954db0d731b9987ad2335d65c01a0
Stasiek99 <stasiekpilich@gmail.com>
2024-02-15 22:09:00 +0100

    project structure refactored

--- src/app/auth/interfaces/login-form-value.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface LoginFormValueInterface {
-  login: string;
-  password: string
-}
--- src/app/views/enums/sidenav-navigation-type.ts
+++
@@ -1,9 +0,0 @@
-export enum SidenavNavigationType {
-  home = "home",
-  search_bar = "search-bar",
-  new_user = "new-user",
-  user_preview = "user-preview",
-  users_list = "users-list",
-  search_history = "search-history",
-  login = "login"
-}
