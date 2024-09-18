22e38ad4604976e3fd0adef8d8b590a5202e1c35
Stasiek99 <stasiekpilich@gmail.com>
2024-09-18 21:05:41 +0200

    httpClient implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from "@angular/forms";
+import { JsonPipe, NgForOf } from "@angular/common";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
 
 import { Country } from "./interfaces/country.interface";
-import { listOfCountries } from "../assets/db.mock";
-import {FormsModule} from "@angular/forms";
-import {NgForOf} from "@angular/common";
+import { listOfCountries } from "../../public/db.mock";
+
+export interface JSONCountry {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, NgForOf],
+  imports: [RouterOutlet, FormsModule, NgForOf, JsonPipe, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'search_bar';
 
+  private readonly jsonURL = "countries.json";
   private readonly urlFragment = "https://www.google.com/search?q=";
 
   filteredCountries: Country[] = [];
   inputValue = "";
+  jsonCountries: JSONCountry[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<(JSONCountry)[]>(this.jsonURL).subscribe(countries => {
+      console.log(countries);
+      this.jsonCountries = countries;
+    });
+  }
 
   onChangesSearchBarInput(searchString: string): void {
     this.filteredCountries = this.getFilteredCountries(searchString);
