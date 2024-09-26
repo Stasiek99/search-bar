adf9b0fbc1fa85b32d87c62a1e7f005f54451f67
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2024-09-26 18:28:13 +0200

    added angular material ui library

--- src/app/new-user/components/user-presentation/user-presentation.component.ts
+++ src/app/new-user/components/user-presentation/user-presentation.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 import { User } from "../../interfaces/user.interface";
+import { NgIf } from "@angular/common";
+import { UserService } from "../../services/user.service";
 
 @Component({
   selector: 'app-user-presentation',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './user-presentation.component.html',
   styleUrl: './user-presentation.component.scss'
 })
 export class UserPresentationComponent implements OnInit {
-  user: User = {
-    name: "Stasiek",
-    login: "Stasiek99",
-    password: "12345",
-    country: "Poland",
-    age: 25
-  }
+  user: User | null = null;
+
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.user = this.userService.getUser() as User;
     console.log(this.user);
   }
 }
