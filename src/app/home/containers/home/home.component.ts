6b3ee552c5cec8c73bd3ab36860bc08d80263614
Stasiek99 <stasiekpilich@gmail.com>
2024-10-08 18:49:16 +0200

    project structure proposal

---
+++ src/app/home/containers/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CountryElement } from "../../../search-engine/feature-search-engine/interfaces/country-element.interface";
+import { CountryDataService } from "../../../search-engine/feature-search-engine/services/country-data.service";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  providers: [CountryDataService],
+  templateUrl: './home.component.html',
+  imports: [
+    NgIf
+  ],
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+  private allCountries: CountryElement[] = [];
+  countriesNames = "";
+  errorOccurred = false;
+
+  constructor(private countryDataService: CountryDataService) {}
+
+  ngOnInit(): void {
+    this.countryDataService.httpGetData().subscribe(countries => {
+      this.allCountries = countries;
+      this.countriesNames = this.allCountries.map((value => {
+        return `${value.name}`
+      })).toString();
+    }, error => {
+      this.errorOccurred = true;
+      console.log(error.message);
+    });
+  }
+}
