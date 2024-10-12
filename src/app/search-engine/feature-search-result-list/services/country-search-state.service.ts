9063b7dcb6bb959efb0f766b368e0e72b61fa157
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2024-10-12 15:08:21 +0200

    feature/unique user search history

--- src/app/search-engine/feature-search-result-list/services/country-search-state.service.ts
+++ src/app/search-engine/feature-search-result-list/services/country-search-state.service.ts
@@ -2,25 +2,49 @@ import { Injectable } from "@angular/core";
 
 import { CountrySearched } from "../interfaces/country-searched.interface";
 import { CountrySearchLocalStorageService } from "./country-search-local-storage.service";
+import { UserStateService } from "../../../users/feature-create-user/services/user-state.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class CountrySearchStateService {
-  private readonly searchedCountries: CountrySearched[];
-  private readonly defaultValue = [];
+  private readonly maxSearchHistory = 15;
+  private searchedCountries: CountrySearched[] = [];
 
-  constructor(private countrySearchLocalStorageService: CountrySearchLocalStorageService) {
-    const tmp: CountrySearched[] | null = this.countrySearchLocalStorageService.getData();
-    this.searchedCountries = tmp ?? this.defaultValue;
+  constructor(private countrySearchLocalStorageService: CountrySearchLocalStorageService, private userStateService: UserStateService) {
+    this.loadUserHistory();
   }
 
   onSubmitted(searchInput: CountrySearched): void {
     this.searchedCountries.push(searchInput);
-    this.countrySearchLocalStorageService.setItem(this.searchedCountries);
+
+    if (this.searchedCountries.length > this.maxSearchHistory) {
+      this.searchedCountries.shift();
+    }
+
+    const userLogin = this.userStateService.getLoggedInUser()?.login;
+    if (userLogin) {
+      this.countrySearchLocalStorageService.setItem(userLogin, this.searchedCountries);
+    }
   }
 
   getSearchedCountries(): CountrySearched[] {
     return this.searchedCountries;
+  }
+
+  clearSearchHistory(): void {
+    const userLogin = this.userStateService.getLoggedInUser()?.login;
+    if (userLogin) {
+      this.countrySearchLocalStorageService.clearSearchHistory(userLogin);
+      this.searchedCountries = [];
+    }
+  }
+
+  private loadUserHistory(): void {
+    const userLogin = this.userStateService.getLoggedInUser()?.login;
+    if (userLogin) {
+      const tmp: CountrySearched[] | null = this.countrySearchLocalStorageService.getData(userLogin);
+      this.searchedCountries = tmp ?? [];
+    }
   }
 }
