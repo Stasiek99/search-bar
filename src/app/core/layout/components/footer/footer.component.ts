2978624488b8a21129c3d02dae3e6bb0b9fb9b51
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2024-10-13 13:55:02 +0200

    theme changed

--- src/app/core/layout/components/footer/footer.component.ts
+++ src/app/core/layout/components/footer/footer.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
-import {MatIconButton} from "@angular/material/button";
-import {MatIcon} from "@angular/material/icon";
+
+import { MatIconButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { MatDivider } from "@angular/material/divider";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
     MatIconButton,
-    MatIcon
+    MatIcon,
+    MatDivider
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  onIconClick (urlString: string): void {
+    window.open(urlString, "_blank");
+  }
 }
